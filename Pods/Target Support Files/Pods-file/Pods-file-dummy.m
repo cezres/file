4e5437d48ece76b07109b4b73cb4433d411310e3
cezres <cezr@sina.com>
2016-10-12 10:36:57 +0800

    d

--- Pods/Target Support Files/Pods-file/Pods-file-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_file : NSObject
-@end
-@implementation PodsDummy_Pods_file
-@end
--- Pods/Target Support Files/Pods-file/Pods-file-umbrella.h
+++
@@ -1,6 +0,0 @@
-#import <UIKit/UIKit.h>
-
-
-FOUNDATION_EXPORT double Pods_fileVersionNumber;
-FOUNDATION_EXPORT const unsigned char Pods_fileVersionString[];
-
