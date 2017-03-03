480d4cdeb2b295f4651aa62d4e772a4d53068169
cezres <cezres@163.com>
2017-03-03 20:07:57 +0800

    HUD相关方法可以在子线程调用

--- Classes/Macros/HUDFunctions.swift
+++ Classes/Macros/HUDFunctions.swift
@@ -10,15 +10,65 @@ import Foundation
 import UIKit
 import MBProgressHUD
 
-func HUDLoading(in view: UIView) -> MBProgressHUD {
-    return MBProgressHUD.showAdded(to: view, animated: true)
+
+@discardableResult func HUDLoading(in view: UIView? = nil) -> MBProgressHUD? {
+    guard Thread.isMainThread else {
+        var result: MBProgressHUD?
+        DispatchQueue.main.sync {
+            result = HUDLoading(in: view)
+        }
+        return result
+    }
+    
+    let inView: UIView
+    if view == nil {
+        if let rootView = UIApplication.shared.keyWindow?.rootViewController?.view {
+            inView = rootView
+        }
+        else {
+            return nil
+        }
+    }
+    else {
+        inView = view!
+    }
+    
+    return MBProgressHUD.showAdded(to: inView, animated: true)
 }
 
-func HUDLoadingHidden(for view: UIView) {
-    MBProgressHUD.hide(for: view, animated: true)
+func HUDLoadingHidden(for view: UIView? = nil) {
+    guard Thread.isMainThread else {
+        DispatchQueue.main.sync {
+            HUDLoadingHidden(for: view)
+        }
+        return
+    }
+    
+    let inView: UIView
+    if view == nil {
+        if let rootView = UIApplication.shared.keyWindow?.rootViewController?.view {
+            inView = rootView
+        }
+        else {
+            return
+        }
+    }
+    else {
+        inView = view!
+    }
+    
+    MBProgressHUD.hide(for: inView, animated: true)
 }
 
 @discardableResult func HUDFailure(message: String, in view: UIView? = nil) -> MBProgressHUD? {
+    guard Thread.isMainThread else {
+        var result: MBProgressHUD?
+        DispatchQueue.main.sync {
+            result = HUDFailure(message: message, in: view)
+        }
+        return result
+    }
+    
     let inView: UIView
     if view == nil {
         if let rootView = UIApplication.shared.keyWindow?.rootViewController?.view {
